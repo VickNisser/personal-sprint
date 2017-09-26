10b993052adc5d180add1ce74aa4fd5605888614
VickNisser <nick__visser@hotmail.com>
2017-09-26 12:57:08 +0200

    added comments and extra node for DOM
    see above

--- PS Grunt/javascript/dom.js
+++ PS Grunt/javascript/dom.js
@@ -6,22 +6,32 @@ function changeNode(nodeToChange, listnum){
 	document.getElementsByClassName(nodeToChange)[listnum].innerHTML = requestedContent;
 }
 
-//add some nodes to the wireframe with a function
+//add some nodes to the wireframe with a function using DOM
 function addNodes(){
+
+	//using appendChild and an ID
 	let listItem = document.createElement('li');
 	listItem.classList.add('introduction__list-item');
 	listItem.innerHTML = 'I was added using DOM';
 	ilist.appendChild(listItem);
 	ilist.setAttribute('style','margin-right:32.5%')
 
+	//using after and an ID
 	let inputField = document.createElement('input');
 	inputField.setAttribute('type','text');
 	inputField.setAttribute('value', 'i was added using DOM');
 	inputField.classList.add("form__text-input");
 	txtarea.after(inputField);
+
+	//using appendChild, the classname and the position in the collection of nodes with that name
+	let footerText = document.createElement('p');
+	footerText.setAttribute('class', 'footer_text');
+	footerText.setAttribute('style', 'color:pink');
+	footerText.innerHTML = 'Some footer text using DOM';
+	let nodeDestination = document.getElementsByClassName('footer')[0];
+	nodeDestination.appendChild(footerText);
+
 }
-
-
 
 changeNode("highlighted-products__view-text", 1);
 addNodes();
